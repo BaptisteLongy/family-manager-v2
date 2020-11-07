7bf4c985d17068c7667849bfea936075d84c79d0
Baptiste Longy <baptiste.longy@gmail.com>
2020-11-07 12:34:29 +0100

    chore: apollo client HOC on the Next app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import '../styles/globals.css'
 import MenuDrawer from '../components/drawer';
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
 
 function MyApp({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
     <div>
-      <MenuDrawer/>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <MenuDrawer />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </div>
   )
 
