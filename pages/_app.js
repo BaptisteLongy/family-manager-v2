4b20a9944467ef87333bd7de22877d24a0a2d235
Baptiste Longy <baptiste.longy@gmail.com>
2020-11-07 10:16:09 +0100

    feat: adding drawer menu architecture

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,48 @@
 import '../styles/globals.css'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(open);
+  };
+
+  return (
+    <div>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={toggleDrawer(true)}
+        edge="start"
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        open={open}
+        onClose={toggleDrawer(false)}
+      >
+              <List>
+        {['Foo', 'Bar'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      </Drawer>
+      <Component {...pageProps} />
+    </div>
+  )
+
 }
 
 export default MyApp
