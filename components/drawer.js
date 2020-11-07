0deae875c5113eb1f03931a8a1a87ca0c537e5fc
Baptiste Longy <baptiste.longy@gmail.com>
2020-11-07 12:35:26 +0100

    feat: displaying all the lists in the drawer

--- components/drawer.js
+++ components/drawer.js
@@ -5,39 +5,53 @@ import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { useQuery } from '@apollo/client'
+import { ALL_LISTS } from '../requests/lists'
+import Link from 'next/link'
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+}
 
 export default function MenuDrawer() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = (open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-    setOpen(open);
-  };
+    const { loading, error, data } = useQuery(ALL_LISTS)
 
-  return (
-    <div>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={toggleDrawer(true)}
-        edge="start"
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer
-        open={open}
-        onClose={toggleDrawer(false)}
-      >
-              <List>
-        {['Foo', 'Bar'].map((text) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      </Drawer>
-    </div>
-  )
+    const toggleDrawer = (open) => (event) => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setOpen(open);
+    };
+
+    if (error) return <p>Erreur :(</p>
+    if (loading) return <p>Loading !</p>
+
+    return (
+        <div>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer(true)}
+                edge="start"
+            >
+                <MenuIcon />
+            </IconButton>
+            <Drawer
+                open={open}
+                onClose={toggleDrawer(false)}
+            >
+                <List>
+                    {data.lists.map((list) => (
+                        <Link href={`/list/${list.id}`} passHref key={list.id}>
+                            <ListItem button component="a" key={list.id}>
+                                <ListItemText primary={list.name} />
+                            </ListItem>
+                        </Link>
+                    ))}
+                </List>
+            </Drawer>
+        </div>
+    )
 }
