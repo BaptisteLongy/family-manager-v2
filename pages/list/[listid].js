03a4b8535d2128240a8b5481ca32e2dec4359f80
Baptiste Longy <baptiste.longy@gmail.com>
2020-11-07 12:35:48 +0100

    feat: displaying the list items in the list page

--- pages/list/[listid].js
+++ pages/list/[listid].js
@@ -1,11 +1,22 @@
 import { useRouter } from 'next/router'
+import { useQuery } from '@apollo/client'
+import { LIST } from '../../requests/lists'
 
 export default function List() {
     const router = useRouter()
     const { listid } = router.query
-    return(
+
+    const { loading, error, data } = useQuery(LIST, { variables: {id: parseInt(listid)} })
+
+    if (error) return <p>Erreur :(</p>
+    if (loading) return <p>Loading !</p>
+
+    return (
         <div>
-            <h1>La liste {listid}</h1>
+            <h1>La liste {data.list.name}</h1>
+            {data.list.items.map(item => {
+                return <p>{item.name}</p>
+            })}
         </div>
     )
 }
