ad1f7571ce4a15e5e7a7a1057a5fedce3404d245
Baptiste Longy <baptiste.longy@gmail.com>
2020-11-07 10:45:42 +0100

    chore: exporting the menu drawer from its own file

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,11 @@
 import '../styles/globals.css'
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import MenuDrawer from '../components/drawer';
 
 function MyApp({ Component, pageProps }) {
-  const [open, setOpen] = React.useState(false);
-
-  const toggleDrawer = (open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-    setOpen(open);
-  };
 
   return (
     <div>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={toggleDrawer(true)}
-        edge="start"
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer
-        open={open}
-        onClose={toggleDrawer(false)}
-      >
-              <List>
-        {['Foo', 'Bar'].map((text) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      </Drawer>
+      <MenuDrawer/>
       <Component {...pageProps} />
     </div>
   )
